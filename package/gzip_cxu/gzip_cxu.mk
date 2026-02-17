027ec1bd9cac200704d673c4e4ffcce93756ee1e
Petro Mozil <mozil.petryk@gmail.com>
2026-02-17 13:25:12 +0200

    Add gzip_cxu
    Signed-off-by: Petro Mozil <mozil.petryk@gmail.com>

--- package/gzip_cxu/gzip_cxu.mk
+++ package/gzip_cxu/gzip_cxu.mk
@@ -4,21 +4,27 @@
 #
 ################################################################################
 
-GZIP_VERSION = c8e167e3e8ae3e89a6d622462b5791c2d2b35266
-GZIP_SITE = https://github.com/pmozil/gzip.git
-GZIP_SITE_METHOD = git
-GZIP_AUTORECONF = YES
+GZIP_CXU_VERSION = c8e167e3e8ae3e89a6d622462b5791c2d2b35266
+GZIP_CXU_SITE = https://github.com/pmozil/gzip.git
+GZIP_CXU_SITE_METHOD = git
+GZIP_CXU_AUTORECONF = YES
+GZIP_CXU_GIT_SUBMODULES = YES
+
+define GZIP_CXU_PRE_CONFIGURE_HOOK
+	cd $(@D) && ./bootstrap --gnulib-srcdir=./gnulib/
+endef
+GZIP_CXU_PRE_CONFIGURE_HOOKS += GZIP_CXU_PRE_CONFIGURE_HOOK
 
 # Some other tools expect it to be in /bin
-GZIP_CONF_OPTS = --exec-prefix=/
+GZIP_CXU_CONF_OPTS = --exec-prefix=/
 
-GZIP_LICENSE = GPL-3.0+
-GZIP_LICENSE_FILES = COPYING
-GZIP_CPE_ID_VENDOR = gnu
+GZIP_CXU_LICENSE = GPL-3.0+
+GZIP_CXU_LICENSE_FILES = COPYING
+GZIP_CXU_CPE_ID_VENDOR = gnu
 
-GZIP_CONF_ENV += gl_cv_func_fflush_stdin=yes
-HOST_GZIP_CONF_ENV += gl_cv_func_fflush_stdin=yes
-GZIP_CONF_ENV += ac_cv_path_shell=/bin/sh
+GZIP_CXU_CONF_ENV += gl_cv_func_fflush_stdin=yes
+HOST_GZIP_CXU_CONF_ENV += gl_cv_func_fflush_stdin=yes
+GZIP_CXU_CONF_ENV += ac_cv_path_shell=/bin/sh
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
